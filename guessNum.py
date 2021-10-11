293c859828b82d469f8eb772153006854edb653d
HansKu <boy7323@gamil.com>
2021-10-11 15:15:31 +0800

    main finish

--- guessNum.py
+++ guessNum.py
@@ -2,4 +2,18 @@ import random
 x = random.randint(1,50)
 print(x)
 
-# y = int(input("Pls key num"))
+for i in range(5):
+    try:
+        y = int(input("Pls key num(1~50)"))
+        if x == y:
+            print("good")
+            break
+        if x > y:
+            print("guess bigger")
+        else:
+            print("guess smaller")
+    except Exception as e:
+        print(e)
+
+
+  
